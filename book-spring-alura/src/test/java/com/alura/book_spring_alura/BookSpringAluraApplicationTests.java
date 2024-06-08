38ff3eb46e5d388275c6322f68859f69eeab52b4
Deleina Llamocca <marta.deleina@gmail.com>
2024-06-08 00:30:17 -0500

    first commit

---
+++ book-spring-alura/src/test/java/com/alura/book_spring_alura/BookSpringAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.book_spring_alura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSpringAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
